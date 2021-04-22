b59a70a6394aaf7ffb7395e4e8c349ae841c0f7e
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-04-21 23:55:14 -0700

    Details vote - part 1

--- Pages/Links/Details.cshtml.cs
+++ Pages/Links/Details.cshtml.cs
@@ -7,17 +7,23 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using LinkAggregator.Data;
 using LinkAggregator.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace LinkAggregator.Pages.Links
 {
     public class DetailsModel : PageModel
     {
-        private readonly LinkAggregator.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private UserManager<IdentityUser> UserManager { get; }
 
-        public DetailsModel(LinkAggregator.Data.ApplicationDbContext context)
+        public DetailsModel(
+            ApplicationDbContext context,
+            UserManager<IdentityUser> userManager)
         {
             _context = context;
+            UserManager = userManager;
         }
+
 
         public Link Link { get; set; }
 
