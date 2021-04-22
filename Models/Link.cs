6710a2267cbc1e32fc663967d2422e947f458010
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-04-21 23:54:37 -0700

    Models\Link.cs - Title field

--- Models/Link.cs
+++ Models/Link.cs
@@ -7,6 +7,7 @@ namespace LinkAggregator.Models
         public int Id { get; set; }
         public string UserId { get; set; }
         public string Url { get; set; }
+        public string Title { get; set; }
         public DateTime DateTime { get; set; }
     }
 }
