29c4da45d03a57ee058fc9514dc5bc8dd0fd3401
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-04-21 23:55:14 -0700

    Details vote - part 2

--- Pages/Links/Details.cshtml.cs
+++ Pages/Links/Details.cshtml.cs
@@ -27,6 +27,8 @@ namespace LinkAggregator.Pages.Links
 
         public Link Link { get; set; }
 
+        public string CurrentUserid() => UserManager.GetUserId(User);
+
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
