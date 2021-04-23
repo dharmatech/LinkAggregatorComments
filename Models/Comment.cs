c7849e39f76a4c91b085e6265d5d6b41826c1afe
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-04-22 19:40:28 -0700

    Comment and CommentVote

---
+++ Models/Comment.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkAggregator.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public int LinkId { get; set; }
+        public Link Link { get; set; }
+
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public IdentityUser User { get; set; }
+
+        public List<CommentVote> Votes { get; set; }
+    }
+}
---
+++ Models/CommentVote.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkAggregator.Models
+{
+    public class CommentVote
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public IdentityUser User { get; set; }
+
+        public int CommentId { get; set; }
+        public Comment Comment { get; set; }
+
+        public int Score { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
