c3f0a79b1d432429238acd75445ff786f1667c84
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-04-22 19:46:30 -0700

    Update ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,5 +16,9 @@ namespace LinkAggregator.Data
         public DbSet<Link> Link { get; set; }
 
         public DbSet<Vote> Vote { get; set; }
+
+        public DbSet<Comment> Comment { get; set; }
+
+        public DbSet<CommentVote> CommentVote { get; set; }
     }
 }
