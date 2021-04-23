04ad860d91eb8c5ad171fa72b819b92d79a20f6b
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-04-22 20:33:18 -0700

    Details - OnPostAddCommentAsync

--- Pages/Links/Details.cshtml.cs
+++ Pages/Links/Details.cshtml.cs
@@ -27,7 +27,7 @@ namespace LinkAggregator.Pages.Links
 
         public Link Link { get; set; }
 
-        public string CurrentUserid() => UserManager.GetUserId(User);
+        public string CurrentUserId() => UserManager.GetUserId(User);
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -39,6 +39,7 @@ namespace LinkAggregator.Pages.Links
             Link = await _context.Link
                 .Include(link => link.User)
                 .Include(link => link.Votes)
+                .Include(link => link.Comments)
                 .FirstOrDefaultAsync(m => m.Id == id);
 
             if (Link == null)
@@ -62,6 +63,25 @@ namespace LinkAggregator.Pages.Links
 
             await link.Vote(score, UserManager.GetUserId(User));
 
+            await _context.SaveChangesAsync();
+
+            return Redirect(HttpContext.Request.Headers["Referer"]);
+        }
+
+        public async Task<IActionResult> OnPostAddCommentAsync(int id, string text)
+        {
+            if (User == null)
+                return Redirect(HttpContext.Request.Headers["Referer"]);
+
+            if (User.Identity.IsAuthenticated == false)
+                return Redirect(HttpContext.Request.Headers["Referer"]);
+                        
+            var link = await _context.Link
+                .Include(link => link.Comments)
+                .FirstOrDefaultAsync(link => link.Id == id);
+                        
+            await link.AddComment(text, CurrentUserId());
+                        
             await _context.SaveChangesAsync();
 
             return Redirect(HttpContext.Request.Headers["Referer"]);
